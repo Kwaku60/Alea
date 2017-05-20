ba8686b9161ecbaf87a97eb5e702ccc0598c9949
Kwaku60 <kwakuklausphoto@gmail.com>
2017-05-20 14:17:04 -0400

    u

--- server.js
+++ server.js
@@ -29,18 +29,6 @@ require("./routes/post-api-routes.js")(app);
 
 
 
-if(process.env.DATABASE_URL){
-
-
-	sequelize = new Sequelize(process.env.DATABASE_URL,{
-		dialect: "postgres",
-		protocol: "postgres",
-		logging: true 
-	});
-} else {
-
-	sequelize = new Sequelize("postgres:///users")''
-}
 
 
 
